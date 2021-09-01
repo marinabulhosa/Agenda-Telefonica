1b83dabf18831ce1ec84ff1ccdd5069a55b2da31
Marina Bulhosa <78749750+marinabulhosa@users.noreply.github.com>
2021-09-01 18:10:54 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -6,7 +6,7 @@ package agendatelef;
 public class Principal {
 
     public static void inicializa(Agenda agendaTelefonica) {
-        								//Cria agenda
+        								
             agendaTelefonica.adicionarContato("Fulano", 99999999, "Rua A", "UFF");                              //Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
             agendaTelefonica.adicionarContato("Ciclano", 88888888, "Rua B", "Cederj");                          //Inserir o Contato "Ciclano", 88888888, "Rua B", "Cederj";
             agendaTelefonica.adicionarContato("Beltrano", 88889999, "Rua C", "Infância");                       //Inserir o Contato "Beltrano", 88889999, "Rua C", "Infância";
